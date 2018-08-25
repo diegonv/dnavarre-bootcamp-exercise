4c2295b7426cf405384871eadc6948769dcad3b9
Diego Navarre <ddn312@gmail.com>
2018-08-25 17:08:49 -0300

    initial commit

---
+++ api/app/Http/Middleware/ResponseMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ResponseMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $responsecode = 200;
+        $header = array(
+            'Content-Type' => 'application/json; charset=utf-8',
+            'charset' => 'utf-8',
+        );
+        return response()->json($response->original, $responsecode, $header);
+    }
+}
